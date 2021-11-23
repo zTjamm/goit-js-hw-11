626e3450521015dfd12ca48ead4f8907d5dc9e45
zTjamm <jamm85@bk.ru>
2021-11-23 09:18:20 +0300

    test
    t

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -6366,7 +6366,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61037" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59961" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
